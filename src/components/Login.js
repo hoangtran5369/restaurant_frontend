3bb8411c36742de3b5af5dc0653206c1037dc456
Quang Nguyen <quang.h.nguyen@sjsu.edu>
2021-06-01 10:09:50 -0700

    refactored login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,29 @@
-import { Button, Card, CardContent, Divider, Grid, TextField, Typography } from "@material-ui/core";
+import { Button, Card, CardContent, Divider, Grid, TextField, Typography, Box } from "@material-ui/core";
 import { Link, useHistory } from "react-router-dom";
 import Navbar from "./Navbar";
-import { Box } from "@material-ui/core";
 import {useForm} from "react-hook-form";
 import {logIn} from "../store/auth/reducer";
 import { useSelector, useDispatch } from 'react-redux'
+import styled from "styled-components";
+
+const InputField = styled(TextField)`
+    margin: 0.5rem 0;
+`
+const ForgotPassLink = styled.a`
+    padding: 10px;
+    display: block;
+    flex-basis: 50%;
+`
+
+const LoginButton = styled(Button)`
+    padding: 10px;
+    flex-basis: 50%;
+`
+
+const SignupPrompt = styled.p`
+    text-align: center;
+    margin: 0.5rem 0;
+`
 
 function Login() {
     const dispatch = useDispatch()
@@ -22,55 +41,28 @@ function Login() {
 
 
     return (
-        <div>
+        <Box minHeight="100vh" flexDirection="column" display="flex">
             <Navbar />
-            <Box display="flex" minHeight="100vh" justifyContent="space-around" alignItems="center">
+            <Box flexGrow={1} display="flex" justifyContent="space-around" alignItems="center">
                 <Box minWidth="300px" maxWidth="400px">
-
                 <Card>
                     <CardContent>
                         <form onSubmit={onSubmit}>
-                            <Grid container spacing={3}>
-                                <Grid item xs={12}> <Typography variant="h5" component="h2" gutterBottom>Sign in</Typography> </Grid>
-                                <Grid item xs={12}>
-                                    <Grid container spacing={2}>
-                                        <Grid item xs={12}>
-                                            <TextField {...register('email')} fullWidth label="Email" name="email" size="small" variant="outlined" />
-                                        </Grid>
-                                        <Grid item xs={12}>
-                                            <TextField {...register('password')} fullWidth label="Password" name="password" size="small" type="password" variant="outlined" />
-                                        </Grid>
-                                    </Grid>
-                                </Grid>
-                                <Grid item container spacing={2} xs={12}>
-                                    <Grid item xs={6}>
-                                        <a>Forgot password?</a>
-                                    </Grid>
-                                    <Grid item xs={6}>
+                            <Typography variant="h5" component="h2" gutterBottom>Sign in</Typography>
+                            <InputField {...register('email')} fullWidth label="Email" name="email" size="small" variant="outlined" />
+                            <InputField {...register('password')} fullWidth label="Password" name="password" size="small" type="password" variant="outlined" />
+                            <Box display="flex">
+                                <ForgotPassLink href="/">Forgot password?</ForgotPassLink>
+                                <LoginButton color="primary" fullWidth type="submit" variant="contained">Log in</LoginButton>
+                            </Box>
 
-                                    <Button color="primary" fullWidth type="submit" variant="contained">
-                                        Log in
-                                    </Button>
-                                    </Grid>
-                                </Grid>
-                                <Divider />
-                                <Grid item xs={12} container direction="row" justify="space-around" alignItems="center">
-                                        <Grid item xs={8}> 
-                                        <Box component="span" m={1}>
-                                        New to Pho28?
-                                        </Box>    
-                                        <Link to="/register">
-                                        Sign up
-                                        </Link>
-                                        </Grid>
-                                </Grid>
-                            </Grid>
+                            <SignupPrompt>New to Pho28? <Link to="/register">Sign up</Link></SignupPrompt>
                         </form>
                     </CardContent>
                 </Card>
                 </Box>
             </Box>
-        </div>
+        </Box>
     );
 }
 
