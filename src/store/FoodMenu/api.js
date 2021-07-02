9883f273ec9022bea8b2a59bc9bb5dc60fea801f
Quang Nguyen <quang.h.nguyen@sjsu.edu>
2021-07-02 15:36:51 -0700

    added environmental variable

--- src/store/FoodMenu/api.js
+++ src/store/FoodMenu/api.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
+const base_url = process.env.REACT_APP_API_URL
+
 export const fetchCategories = async () => {
-    const resp = await axios.get("http://192.168.0.119:8080/v0/category");
+    const resp = await axios.get(`${base_url}/category`);
     return resp.data;
 }
 
 export const fetchFoodMenuItems = async () => {
-    const resp = await axios.get("http://192.168.0.119:8080/v0/menu-item");
+    const resp = await axios.get(`${base_url}/menu-item`);
     return resp.data;
 }
 
