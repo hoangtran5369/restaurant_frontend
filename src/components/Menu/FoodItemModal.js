f6904ddde140927ea58b3e9b39f041acfa6b61e1
Quang Nguyen <quang.h.nguyen@sjsu.edu>
2021-07-05 12:11:00 -0700

    added css for modal

--- src/components/Menu/FoodItemModal.js
+++ src/components/Menu/FoodItemModal.js
@@ -11,47 +11,47 @@ import {
     Checkbox,
     TextField,
     Button,
-  } from "@material-ui/core";
-  import styled from "styled-components";
-  import { useDispatch, useSelector } from "react-redux";
-  import { itemIsDisplayed, getDisplayedItem } from "store/FoodMenu/selector";
-  import { hideItem } from "store/FoodMenu/reducer";
-  import React, { useState } from "react";
-  import { addOrder } from "store/Order/reducer";
-  
-  const MyModal = styled(Modal)`
+    Typography,
+    FormLabel,
+    Divider,
+} from "@material-ui/core";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { itemIsDisplayed, getDisplayedItem } from "store/FoodMenu/selector";
+import { hideItem } from "store/FoodMenu/reducer";
+import React, { useState } from "react";
+import { addOrder } from "store/Order/reducer";
+
+const MyModal = styled(Modal)`
     margin: auto;
     width: 70vw;
     min-width: 600px;
     background-color: white;
-    padding: 2rem;
     box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
       rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
       rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
   `;
-  
-  const MainContainer = styled.div`
-    background-color: blue;
+
+const MainContainer = styled.div`
     display: flex;
-   
+    padding: 2rem;
+    height: 100%;
   `;
-  
-  const LeftContainer = styled.div`
-    background-color: red;
+
+const LeftContainer = styled.div`
     width: 70%;
      
   `;
-  
-  const RightContainer = styled.div`
-    
+
+const RightContainer = styled.div`
     width: 30%;
-    background-color: yellow;
   `;
-  
-  const ImageContainer = styled.div`
+
+const ImageContainer = styled.div`
     width: 100%;
-    max-width: 500px;
-    background-color: green;
+    max-width: 600px;
+    border: 1px solid rgb(205,205,205);
+    border-radius: 25px;
   
     &:after {
       content: "";
@@ -59,134 +59,133 @@ import {
       padding-bottom: 100%;
     }
   `;
-  const ImageListContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    min-width: 100px;
-    background-color: blue;
-  `;
-  const GalleryList = styled(GridList)`
+
+const GalleryList = styled(GridList)`
+    margin: 10px;
     max-height: 580px;
   `;
-  const QuantityPicker = styled(TextField)`
+const QuantityPicker = styled(TextField)`
     width: 4rem;
   `;
-  
-  const OrderButton = styled(Button)`
-    margin: 0 1rem;
+
+const OrderButton = styled(Button)`
+    margin-left: 1rem !important;
+
   `;
-  
-  function FoodItemModal() {
+
+const PreviewContainer = styled.div`
+    display: block;
+    padding-bottom: 100%;
+    position: relative;
+    border-radius: 20px;
+    overflow: hidden;
+`
+
+const PreviewImage = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: absolute;
+`
+
+function FoodItemModal() {
     const modalOpen = useSelector(itemIsDisplayed);
     const item = useSelector(getDisplayedItem);
     const dispatch = useDispatch();
     const [quantity, updateQuantity] = useState(1);
     const handleOrder = () => {
-      dispatch(addOrder({ item, quantity }));
-      dispatch(hideItem());
+        dispatch(addOrder({ item, quantity }));
+        dispatch(hideItem());
     };
     const options = ["Khong gia", "Khong hanh", "Khong Thit"];
-  
-    return (
-      <MyModal
-        BackdropProps={{ invisible: true }}
-        open={modalOpen}
-        onClose={() => dispatch(hideItem())}
-      >
-        <MainContainer>
-          {/* <h3>{JSON.stringify(currDisplayedItem)}</h3> */}
-  
-          <LeftContainer>
-            {/* <Box display="flex" height="500px">
-                      <ImageListContainer>
-                      </ImageListContainer>
-                      <ImageContainer>
-                      </ImageContainer>
-                      </Box> */}
-            <Grid
-              container
-              direction="row"
-              justify="flex-start"
-              alignItems="stretch"             
-            >
-              <Grid item xs={2}>
-                <GalleryList cellHeight={100} cols={1}>
-                  <GridListTile>
-                    <img src="https://i.imgur.com/yGeOUMB.jpg" alt="" />
-                  </GridListTile>
-                  <GridListTile>
-                    <img src="https://i.imgur.com/yGeOUMB.jpg" alt="" />
-                  </GridListTile>
-                  {/* <GridListTile>
-                                      <img src="https://i.imgur.com/yGeOUMB.jpg" alt="" />
-                                  </GridListTile>
-                                  <GridListTile>
-                                      <img src="https://i.imgur.com/yGeOUMB.jpg" alt="" />
-                                  </GridListTile>
-                                  <GridListTile>
-                                      <img src="https://i.imgur.com/yGeOUMB.jpg" alt="" />
-                                  </GridListTile>
-                                  <GridListTile>
-                                      <img src="https://i.imgur.com/yGeOUMB.jpg" alt="" />
-                                  </GridListTile>
-                                  <GridListTile>
-                                      <img src="https://i.imgur.com/yGeOUMB.jpg" alt="" />
-                                  </GridListTile> */}
-                </GalleryList>
-              </Grid>
-              <Grid item xs={9}>
-                <ImageContainer></ImageContainer>
-              </Grid>
-            </Grid>
-            
-          </LeftContainer>
-          <RightContainer>        
 
-            <h2> {item && item.name}</h2>
-            <h2> ${item && item.price} </h2>
-            <h2>  {item && item.description} </h2>
-            <h2> Option: </h2>
-  
-            {options.map((option) => (
-              <div>
-                <FormControlLabel
-                  control={<Checkbox name="checkedB" color="primary" />}
-                  label={option}
-                />{" "}
-              </div>
-            ))}
-  
-            <TextField
-              label="Special instruction:"
-              id="outlined-margin-none"
-              defaultValue="Default Value"
-              helperText="150 characters maximum."
-              variant="outlined"
-            />
-            <Box display="flex" alignContent="stretch">
-              <QuantityPicker
-                label="Count"
-                value={quantity}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                type="number"
-                defaultValue={1}
-                variant="outlined"
-                size="small"
-                onChange={(event) => updateQuantity(event.target.value)}
-              />
-              <OrderButton variant="contained" onClick={handleOrder} color="primary"  >
-                Add to card
+    return (
+        <MyModal
+            BackdropProps={{ invisible: true }}
+            open={modalOpen}
+            onClose={() => dispatch(hideItem())}
+        >
+            <MainContainer>
+
+                <LeftContainer>
+
+                    <Grid
+                        container
+                        direction="row"
+                        justify="space-around"
+                        alignItems="stretch"
+                    >
+                        <Grid item xs={2}>
+                            <GalleryList cellHeight="auto" cols={1}>
+                                <GridListTile>
+                                    <PreviewContainer>
+                                    <PreviewImage src="https://i.imgur.com/yGeOUMB.jpg" alt="" />
+                                    </PreviewContainer>
+                                    
+                                </GridListTile>
+
+                            </GalleryList>
+                        </Grid>
+                        <Grid item xs={9}>
+                            <ImageContainer></ImageContainer>
+                        </Grid>
+                    </Grid>
+
+                </LeftContainer>
+                <RightContainer>
+                    <Typography variant="h4">{item && item.name}</Typography>
+                    <Typography variant="h5" gutterBottom>${item && item.price}</Typography>
+                    <Typography variant="body1" >{item && item.description}</Typography>
+                    <Box marginY="20px">
+
+
+                    <FormControl component="fieldset">
+                    <FormLabel component="legend">Item options</FormLabel>
+                    {options.map((option) => (
+                        <div>
+                            <FormControlLabel
+                                control={<Checkbox name="checkedB" color="primary" />}
+                                label={option}
+                            />{" "}
+                        </div>
+                    ))}
+
+                    
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Special instructions"
+                        multiline
+                        rows={2}
+                        variant="outlined"
+                    />
+                    </FormControl>
+                    </Box>
+
+                        
+                    <Divider />
+
+                    <Box display="flex" alignContent="stretch" paddingTop="20px">
+                        <QuantityPicker
+                            label="Count"
+                            value={quantity}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            type="number"
+                            defaultValue={1}
+                            variant="outlined"
+                            onChange={(event) => updateQuantity(event.target.value)}
+                        />
+                        <OrderButton variant="contained" onClick={handleOrder} color="primary"  >
+                            Add to cart
               </OrderButton>
-            </Box>
-           
-             
-          </RightContainer>
-        </MainContainer>
-      </MyModal>
+                    </Box>
+
+
+                </RightContainer>
+            </MainContainer>
+        </MyModal>
     );
-  }
-  
-  export default FoodItemModal;
-  
+}
+
+export default FoodItemModal;
