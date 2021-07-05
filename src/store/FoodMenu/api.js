49da48ba2a468c0b0f199a9c49682a4a9d7cb8f1
Quang Nguyen <quang.h.nguyen@sjsu.edu>
2021-07-05 12:16:20 -0700

    added api object

--- src/store/FoodMenu/api.js
+++ src/store/FoodMenu/api.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
+import api, {FoodMenuUrl} from 'store/api';
 
 const base_url = process.env.REACT_APP_API_URL
 
 export const fetchCategories = async () => {
-    const resp = await axios.get(`${base_url}/category`);
+    const resp = await api.get(FoodMenuUrl.getCategories);
     return resp.data;
 }
 
 export const fetchFoodMenuItems = async () => {
-    const resp = await axios.get(`${base_url}/menu-item`);
+    const resp = await api.get(FoodMenuUrl.getMenuItems);
     return resp.data;
 }
 
